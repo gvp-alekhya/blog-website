0f23265eaac0c085347750f11ae47d4b044a843c
unknown <orugantialekhya15@gmail.com>
2022-12-16 15:10:01 -0800

    Adding docker image build

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {JSON.stringify(allPostsData)}
+         
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>{title}</Link>
---
+++ scripts/after_install.sh
@@ -0,0 +1,5 @@
+ 
+#!/bin/bash
+cd /home/ec2-user/app-frontend
+npm install
+npm install pm2 -g
